9e42d4c9d057ecfe9e856f6cebcdcf950f6bb916
FlyingSpaten <jfrese@web.de>
2015-02-18 13:39:16 +0100

    Beginning Thermal implementation. Added HeightGain(), StartHeight() and EndHeight() to Thermal interface.

--- IThermal.go
+++ IThermal.go
@@ -5,6 +5,9 @@ import "time"
 type Thermal interface {
 	Records() []IgcRecord
 	AverageStrength() float64
+	HeightGain() int64
+	StartHeight() int64
+	EndHeight() int64
 	Date() time.Time
 	Duration() time.Duration
 	WindDir() int64
---
+++ ThermalImpl.go
@@ -0,0 +1,35 @@
+package raspberryFly
+
+import (
+	"time"
+)
+
+type ThermalImpl struct {
+	ThermalRecords []IgcRecord
+}
+
+func (t ThermalImpl) Records() []IgcRecord {
+	return t.ThermalRecords
+}
+
+func (t ThermalImpl) AverageStrength() float64 {
+	first := t.ThermalRecords[0]
+	last := t.ThermalRecords[len(t.ThermalRecords)-1]
+
+	time := last.Time().Sub(first.Time()).Seconds()
+
+	heightGainGps := last.GpsAltitude() - first.GpsAltitude()
+	heightGainPreassure := last.PreassureAltitude() - first.PreassureAltitude()
+
+	averageHeightGain := float64(heightGainGps+heightGainPreassure) / 2.0
+
+	return averageHeightGain / time
+}
+
+func (t ThermalImpl) Time() time.Time {
+	return t.ThermalRecords[0].Time()
+}
+
+func (t ThermalImpl) Duration() time.Duration {
+	return t.ThermalRecords[len(t.ThermalRecords)-1].Time().Sub(t.ThermalRecords[0].Time())
+}
